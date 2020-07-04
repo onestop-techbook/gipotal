c365515edccb0c370d0ec6b1dc417d8a3b9f4759
nabe-kurage <tabiflag@gmail.com>
2020-07-04 16:00:51 +0900

    hasura導入

---
+++ src/components/profiles.js
@@ -0,0 +1,29 @@
+import gql from 'graphql-tag';
+import {useQuery} from '@apollo/react-hooks';
+
+const OUR_FIRST_QUERY = gql`
+  query {
+    profile {
+      id
+      name
+    }
+  }
+`;
+
+const Profiles = () => {
+  const { loading, error, data } = useQuery(OUR_FIRST_QUERY)
+
+  if (loading) return <p>loading</p>
+  if (error) return <p>{error.toString()}</p>
+  return <div>
+    {
+      data.profile.map(profile => <Profile key={profile.id} profile={profile}/>)
+    }
+  </div>
+}
+
+const Profile = ({ profile }) => {
+  return <p>{profile.id}: {profile.name}</p>  
+}
+
+export default Profiles
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,40 @@
 import Head from 'next/head'
 import styles from './index.module.css'
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import Profiles from '../components/profiles'
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'http://localhost:8080/v1/graphql'
+    }),
+    cache: new InMemoryCache(),
+  });
+ };
 
 export default function Home() {
+  const client = createApolloClient();
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.hello}>
-      ギポッ
-      </main>
+        <main className={styles.hello}>
+          ギポッ
+        </main>
 
-      <footer>
-      </footer>
+        <Profiles/>
 
-    
-    </div>
+        <footer>
+        </footer>
+      </div>
+    </ApolloProvider>
   )
 }
