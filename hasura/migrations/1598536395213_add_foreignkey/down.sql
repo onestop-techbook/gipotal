9fb7496457bb74e1b3e8ae1b6d2eff0908dd43e9
nabe-kurage <tabiflag@gmail.com>
2020-08-27 22:54:59 +0900

    foreignkeyを追加

---
+++ hasura/migrations/1598536395213_add_foreignkey/down.sql
@@ -0,0 +1,35 @@
+
+
+
+alter table "public"."tagging" drop constraint "tagging_item_id_fkey";
+
+alter table "public"."tagging" drop constraint "tagging_tag_id_fkey";
+
+alter table "public"."circle_links" drop constraint "circle_links_circle_id_fkey";
+
+alter table "public"."circle_items" drop constraint "circle_items_genre_id_fkey";
+
+alter table "public"."tagging" drop constraint "tagging_item_id_fkey";
+
+alter table "public"."tagging" drop constraint "tagging_tag_id_fkey";
+
+alter table "public"."circle_links" drop constraint "circle_links_circle_id_fkey";
+
+alter table "public"."circle_items" drop constraint "circle_items_genre_id_fkey";
+
+
+alter table "public"."tagging" drop constraint "tagging_item_id_fkey";
+
+alter table "public"."tagging" drop constraint "tagging_tag_id_fkey";
+
+alter table "public"."circle_links" drop constraint "circle_links_circle_id_fkey";
+
+alter table "public"."circle_items" drop constraint "circle_items_genre_id_fkey";
+
+alter table "public"."tagging" drop constraint "tagging_item_id_fkey";
+
+alter table "public"."tagging" drop constraint "tagging_tag_id_fkey";
+
+alter table "public"."circle_links" drop constraint "circle_links_circle_id_fkey";
+
+alter table "public"."circle_items" drop constraint "circle_items_genre_id_fkey";
---
+++ hasura/migrations/1598536395213_add_foreignkey/up.sql
@@ -0,0 +1,99 @@
+
+alter table "public"."circle_items"
+           add constraint "circle_items_genre_id_fkey"
+           foreign key ("genre_id")
+           references "public"."genre"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."circle_links"
+           add constraint "circle_links_circle_id_fkey"
+           foreign key ("circle_id")
+           references "public"."circles"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_tag_id_fkey"
+           foreign key ("tag_id")
+           references "public"."tag"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_item_id_fkey"
+           foreign key ("item_id")
+           references "public"."circle_items"
+           ("id") on update restrict on delete restrict;
+
+
+alter table "public"."circle_items"
+           add constraint "circle_items_genre_id_fkey"
+           foreign key ("genre_id")
+           references "public"."genre"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."circle_links"
+           add constraint "circle_links_circle_id_fkey"
+           foreign key ("circle_id")
+           references "public"."circles"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_tag_id_fkey"
+           foreign key ("tag_id")
+           references "public"."tag"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_item_id_fkey"
+           foreign key ("item_id")
+           references "public"."circle_items"
+           ("id") on update restrict on delete restrict;
+
+
+alter table "public"."circle_items"
+           add constraint "circle_items_genre_id_fkey"
+           foreign key ("genre_id")
+           references "public"."genre"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."circle_links"
+           add constraint "circle_links_circle_id_fkey"
+           foreign key ("circle_id")
+           references "public"."circles"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_tag_id_fkey"
+           foreign key ("tag_id")
+           references "public"."tag"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_item_id_fkey"
+           foreign key ("item_id")
+           references "public"."circle_items"
+           ("id") on update restrict on delete restrict;
+
+
+alter table "public"."circle_items"
+           add constraint "circle_items_genre_id_fkey"
+           foreign key ("genre_id")
+           references "public"."genre"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."circle_links"
+           add constraint "circle_links_circle_id_fkey"
+           foreign key ("circle_id")
+           references "public"."circles"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_tag_id_fkey"
+           foreign key ("tag_id")
+           references "public"."tag"
+           ("id") on update restrict on delete restrict;
+
+alter table "public"."tagging"
+           add constraint "tagging_item_id_fkey"
+           foreign key ("item_id")
+           references "public"."circle_items"
+           ("id") on update restrict on delete restrict;
