ac5bf845de90e2bb7c1859e722bea7ed62dc3c86
SASAKI.Shunsuke <erukiti+github.repo@gmail.com>
2021-09-18 16:13:19 +0900

    ダミーデータを修正

--- src/logics/circles/index.ts
+++ src/logics/circles/index.ts
@@ -10,11 +10,11 @@ export const useGetCircleById = (id: number) => {
             "親方Projectでは、エンジニアの困ったを解決する合同誌を作ってます。見積り何もわからん、を集めた「ワンストップ見積もり」、勉強会は楽しいぞ！を集めたワンストップ勉強会をはじめとして、エンジニアの困ったを解決すべく活動中です。執筆者15名(平均)の知見をあなたにワンストップでお届けます。また、執筆者は絶賛募集中。1ページからでも寄稿OKです。あなたの知見を、今、この本に！書いてみたいと思ったときが書くときです。",
           circleItems: [
             {
-              category: "カテゴリ",
+              category: "ソフトスキル",
               imagePath: "/images/circle_item.png",
-              title: "タイトル",
-              eventName: "イベント名",
-              publishedAt: new Date(),
+              title: "ワンストップ勉強会",
+              eventName: "技術書典6",
+              publishedAt: new Date("2018/04/24"),
               path: "path",
             },
           ],
